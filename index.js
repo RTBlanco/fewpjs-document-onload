51a8193b9ef49b4358a25d00a7364c06411ca453
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-09 12:52:09 -0400

    commit tree b7d57846fc13860f466326a4d40bc1c6a648a14e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentloaded", ()=>{
+  const p = document.getElementById("text");
+  p.innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.' to equal 'This is really cool!"
+})
